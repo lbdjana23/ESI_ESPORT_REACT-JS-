431710b13f620df0e2f4677c7d4de42fbb9a279d
lbdjana23 <ngurahlabda@gmail.com>
2021-12-06 11:23:39 +0800

    membuat company proile

---
+++ Desktop/Github/utsmblapp/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+void main() => runApp(MyApp());class MyApp extends StatelessWidget {
+Widget build(BuildContext context) {
+return MaterialApp(
+home: Scaffold(
+appBar: AppBar(
+title: Text('My first App'),
+),
+body: Center(child: Text('Hello world')),
+),
+);
+}
+}
